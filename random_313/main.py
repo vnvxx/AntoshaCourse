82e1ec718034fd7815dcd5764925f46544789582
vnvxx <vnvnvnkaa@gmail.com>
2024-10-25 23:39:56 +0300

    MyWebFlaskProject

--- random_313/main.py
+++ random_313/main.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title: str, author: str, year: str):
         self.title = title
         self.author = author
         self.year = year
